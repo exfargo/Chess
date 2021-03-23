77691d1ab994cf9a36929933c332aec531bd4a32
aznoH333 <62841387+aznoH333@users.noreply.github.com>
2021-03-23 11:52:52 +0100

    zobrazeni figurek ve hre (zatim nekomunikuje s api)

---
+++ Frontend/src/data/piece.ts
@@ -0,0 +1,26 @@
+import {Teams} from './teams';
+
+export class Piece{
+
+  constructor(private name: string, private htmlText: string, private team: Teams) {
+  }
+  getName(): string{
+    return this.name;
+  }
+  getHtml(): string{
+    let output: string = this.htmlText;
+    if (this.team !== Teams.Empty){
+      // fuj enumy to neumi
+      // enum.tostring vraci index toho enumu a ne ten text
+      if(this.team === Teams.Black){
+        output += ' black-piece';
+      }else {
+        output += ' white-piece';
+      }
+    }
+    return output;
+  }
+  getTeam(): Teams{
+    return this.team;
+  }
+}
