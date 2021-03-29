b7fbf66ec924e3adb93e90cf74685dd8d54c92b1
aznoH333 <62841387+aznoH333@users.noreply.github.com>
2021-03-29 09:24:16 +0200

    temporary absolutně nefunkční

--- Frontend/src/app/game/game.component.ts
+++ Frontend/src/app/game/game.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit} from '@angular/core';
 import {Piece} from '../../data/piece';
 import {Teams} from '../../data/teams';
 
@@ -11,6 +11,8 @@ export class GameComponent implements OnInit {
   // wtf co je za pojmenovani checkboard
   // i blame karel
   checkboard: Piece[][];
+  selectedPiece: Piece;
+  board: ElementRef;
   constructor() {
   }
   ngOnInit(): void {
@@ -25,6 +27,32 @@ export class GameComponent implements OnInit {
     this.checkboard[1][0] = new Piece('knight', 'fas fa-chess-knight', Teams.Black);
     this.checkboard[2][0] = new Piece('king', 'fas fa-chess-king', Teams.White);
 
+  }
+
+  clickedTile(x: number, y: number): void {
+    if (this.selectedPiece === null){
+      if (this.checkboard[x][y].getName() !== 'empty'){
+        this.selectedPiece = this.selectPiece(x, y);
+      }
+    }else {
+        this.selectedPiece = this.selectPiece(null, null);
+    }
+  }
+
+  selectPiece(x: number, y: number): Piece{
+    const temp = this.board.nativeElement.children;
+    // unselect piece
+    // tslint:disable-next-line:prefer-for-of
+    for (let x2 = 0; x2 < temp.length; x2++){
+      for (let y2 = 0; y2 < temp.length; y2++){
+        temp[x2].nativeElement.children[y2].classList = '';
+      }
+    }
+
+    if (x === null || y === null){
+      return null;
+    }
+    return this.checkboard[x][y];
   }
 }
 
