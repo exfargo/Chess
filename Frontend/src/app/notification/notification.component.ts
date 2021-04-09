f76a3779e46835f69180648dbcc74a0233395b86
pavel <pavel.sedlacek2004@gmail.com>
2021-04-09 10:50:47 +0200

    fastFix4

---
+++ Frontend/src/app/notification/notification.component.ts
@@ -0,0 +1,29 @@
+import {Component, OnInit} from '@angular/core';
+import {NotificationService} from '../services/notification.service';
+import {Notification} from '../../data/notification';
+
+@Component({
+             selector: 'app-notification',
+             templateUrl: './notification.component.html',
+             styleUrls: ['./notification.component.scss']
+           })
+export class NotificationComponent implements OnInit {
+  notification: Notification;
+
+  constructor(private notificationService: NotificationService) {
+  }
+
+  ngOnInit(): void {
+    this.notificationService.activeNotification.subscribe(data => {
+      this.notification = data;
+    });
+  }
+
+  getStatus(): boolean {
+    return this.notification.status;
+  }
+
+  cancel(): void {
+    this.notification = null;
+  }
+}
